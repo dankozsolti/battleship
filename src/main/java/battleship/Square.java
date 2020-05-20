5000f7abc203a26be8d3fe6cac7829a30d98e44f
dankozsolti <dankozsolti@freemail.hu>
2020-05-20 22:10:37 +0200

    Updated the fxml resources, redesigned the game scene

---
+++ src/main/java/battleship/Square.java
@@ -0,0 +1,12 @@
+package battleship;
+
+public enum Square {
+
+    EMPTY,
+    SQUARE1,
+    SQUARE2,
+    SQUARE3,
+    SQUARE4,
+    SQUARE5;
+
+}
--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -25,4 +25,5 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GameController {
+
 }
