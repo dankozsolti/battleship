0ef1bdc7c61e97acdf8db96d1b545e21e83d742b
dankozsolti <dankozsolti@freemail.hu>
2020-05-21 17:27:26 +0200

    Added log4j2.xml, made one more GridPane, modified GameController

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -12,6 +12,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 //import battleship.results.GameResult;
@@ -24,6 +25,57 @@ import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
 
+@Slf4j
 public class GameController {
 
+    private String userName;
+    private int stepCount;
+    private List<Image> squareImages;
+    private Instant beginGame;
+
+    @FXML
+    private Label usernameLabel;
+
+    @FXML
+    private GridPane ownGrid;
+
+    @FXML
+    private GridPane enemyGrid;
+
+    @FXML
+    private Label stepLabel;
+
+    @FXML
+    private Label solvedLabel;
+
+    @FXML
+    private Button doneButton;
+
+    public void initdata(String userName) {
+        this.userName = userName;
+        usernameLabel.setText("Current user: " + this.userName);
+    }
+
+    public void squareClickOwn(MouseEvent mouseEvent) {
+
+
+        int clickedColumn = ownGrid.getColumnIndex((Node)mouseEvent.getSource());
+        int clickedRow = ownGrid.getRowIndex((Node)mouseEvent.getSource());
+        System.out.println("own grid, (" + clickedColumn + "," + clickedRow + ")");
+
+
+
+    }
+
+    public void squareClickEnemy(MouseEvent mouseEvent) {
+
+
+        int clickedColumn = enemyGrid.getColumnIndex((Node)mouseEvent.getSource());
+        int clickedRow = enemyGrid.getRowIndex((Node)mouseEvent.getSource());
+
+        System.out.println("enemy grid, (" + clickedColumn + "," + clickedRow + ")");
+
+
+
+    }
 }
