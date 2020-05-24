20d0d5f01470d87226789dd5e6baf03ec3a94e0d
dankozsolti <dankozsolti@freemail.hu>
2020-05-24 20:08:04 +0200

    Added placement logic and toggle button functionality

--- src/main/java/battleship/Ship.java
+++ src/main/java/battleship/Ship.java
@@ -1,14 +1,43 @@
 package battleship;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 
 @Data
 @AllArgsConstructor
 public class Ship {
+
     private int size;
     private int direction;
 
     private int x;
     private int y;
+
+    private int getHeight() {
+        if (this.direction == 1)
+            return this.size + 1;
+        return 1 + 1;
+    }
+
+    private int getWidth() {
+        if (this.direction == 1)
+            return 1 + 1;
+        return this.size + 1;
+    }
+
+    public boolean inShip(Ship other) {
+        float xmin = Math.max(this.x, other.x);
+        float xmax1 = this.x + this.getWidth();
+        float xmax2 = other.x + other.getWidth();
+        float xmax = Math.min(xmax1, xmax2);
+        if (xmax > xmin) {
+            float ymin = Math.max(this.y, other.y);
+            float ymax1 = this.y + this.getHeight();
+            float ymax2 = other.y + other.getHeight();
+            float ymax = Math.min(ymax1, ymax2);
+            if (ymax > ymin) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -2,71 +2,74 @@ package controller;
 
 import battleship.Ship;
 import battleship.Square;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
-//import battleship.results.GameResult;
-//import battleship.results.GameResultDao;
-//import battleship.state.RollingCubesState;
+import org.w3c.dom.css.Rect;
 
-import java.io.IOException;
-import java.time.Duration;
-import java.time.Instant;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 @Slf4j
 public class GameController {
 
-    private String userName1;
-    private String userName2;
     @FXML
     private Label usernameLabel1;
+
     @FXML
     private Label usernameLabel2;
 
     @FXML
     private GridPane ownGrid;
 
+    @FXML
+    private GridPane enemyGrid;
+
+    @FXML
+    private ToggleButton vertical;
+
+    @FXML
+    private ToggleButton horizontal;
+
+
     private List<Ship> ownShips;
+    private List<Ship> enemyShips;
     private HashMap<Square, Image> squares;
 
     @FXML
     public void initialize() {
         ownShips = new ArrayList<Ship>();
+        enemyShips = new ArrayList<Ship>();
         squares = new HashMap<Square, Image>();
 
         squares.put(Square.SQUARE0, new Image(this.getClass().getResource("/img/square0.png").toExternalForm()));
         squares.put(Square.SQUARE1, new Image(this.getClass().getResource("/img/square1.png").toExternalForm()));
         squares.put(Square.SQUARE2, new Image(this.getClass().getResource("/img/square2.png").toExternalForm()));
         squares.put(Square.SQUARE3, new Image(this.getClass().getResource("/img/square3.png").toExternalForm()));
+
+        vertical.setDisable(true);
     }
 
     public void initdata(String userName1, String userName2) {
-        this.userName1 = userName1;
-        this.userName2 = userName2;
-        usernameLabel1.setText(this.userName1);
-        usernameLabel2.setText(this.userName2);
+        usernameLabel1.setText(userName1);
+        usernameLabel2.setText(userName2);
     }
 
-    private void drawOwnShips() {
+    private void drawOwnShips(){
         for (Ship s : ownShips) {
             int startX = s.getX() * 10 + s.getY();
             for (int x = 0; x < s.getSize(); x++) {
+
                 int index = s.getDirection() == 1 ? startX + x : startX + (10 * x);
                 if (index >= 100)
                     continue;
@@ -77,13 +80,48 @@ public class GameController {
         }
     }
 
+    private void drawEnemyShips(){
+        for (Ship s : enemyShips) {
+            int startX = s.getX() * 10 + s.getY();
+            for (int x = 0; x < s.getSize(); x++) {
+                int index = s.getDirection() == 1 ? startX + x : startX + (10 * x);
+                if (index >= 100)
+                    continue;
+
+                ImageView view = (ImageView) enemyGrid.getChildren().get(index);
+                view.setImage(squares.get(Square.SQUARE2));
+            }
+        }
+    }
+
+    private boolean isEmptySpace(List<Ship> ships, Ship ship) {
+        if (horizontal.isDisable()) {
+            if (10 - ship.getY() < ship.getSize()) {
+                log.warn("Ship cant fit..");
+                return false;
+            }
+        } else {
+            if (10 - ship.getX() < ship.getSize()) {
+                log.warn("Ship cant fit..");
+                return false;
+            }
+        }
+
+        for (Ship s : ships) {
+            if (s.inShip(ship))
+                return false;
+        }
+        return true;
+    }
+
+
     public void squareClickOwn(MouseEvent mouseEvent) {
 
         int clickedColumn = GridPane.getColumnIndex((Node)mouseEvent.getSource());
         int clickedRow = GridPane.getRowIndex((Node)mouseEvent.getSource());
         System.out.println("own grid, (" + clickedRow + "," + clickedColumn + ")");
 
-        Ship ship = new Ship(3, 2,clickedRow, clickedColumn );
+        Ship ship = new Ship(3, horizontal.isDisable() ? 1 : 2,clickedRow, clickedColumn);
         ownShips.add(ship);
 
         drawOwnShips();
@@ -96,5 +134,36 @@ public class GameController {
 
         System.out.println("enemy grid, (" + clickedRow + "," + clickedColumn + ")");
 
+        int size = 3;
+        Ship ship = new Ship(size, horizontal.isDisable() ? 1 : 2, clickedRow, clickedColumn);
+        if (!isEmptySpace(enemyShips, ship)) {
+            log.warn("Ship in line!!");
+            return;
+        }
+        enemyShips.add(ship);
+
+        drawEnemyShips();
+    }
+
+    public void squareHoverOwn(){
+        System.out.println("Hovered own");
+    }
+
+    public void squareHoverEnemy(){
+        System.out.println("Hovered enemy");
+    }
+
+    public void horizontalRelease() {
+
+        vertical.setDisable(false);
+        horizontal.setDisable(true);
+        vertical.setSelected(false);
+    }
+
+    public void verticalRelease(){
+
+        horizontal.setDisable(false);
+        vertical.setDisable(true);
+        horizontal.setSelected(false);
     }
 }
