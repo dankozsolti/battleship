e30d56bc1e981b8d48e13f5ec89711fcc5d3bf65
dankozsolti <dankozsolti@freemail.hu>
2020-05-26 17:49:56 +0200

    Added basic guessing part on enemy grid

--- src/main/java/battleship/Ship.java
+++ src/main/java/battleship/Ship.java
@@ -12,27 +12,27 @@ public class Ship {
     private int x;
     private int y;
 
-    private int getHeight() {
+    private int getHeight(boolean isSpace) {
         if (this.direction == 1)
-            return this.size + 1;
-        return 1 + 1;
+            return this.size + (isSpace ? 1 : 0);
+        return 1 + (isSpace ? 1 : 0);
     }
 
-    private int getWidth() {
+    private int getWidth(boolean isSpace) {
         if (this.direction == 1)
-            return 1 + 1;
-        return this.size + 1;
+            return 1 + (isSpace ? 1 : 0);
+        return this.size + (isSpace ? 1 : 0);
     }
 
-    public boolean inShip(Ship other) {
+    public boolean inShip(Ship other, boolean isSpace) {
         float xmin = Math.max(this.x, other.x);
-        float xmax1 = this.x + this.getWidth();
-        float xmax2 = other.x + other.getWidth();
+        float xmax1 = this.x + this.getWidth(isSpace);
+        float xmax2 = other.x + other.getWidth(isSpace);
         float xmax = Math.min(xmax1, xmax2);
         if (xmax > xmin) {
             float ymin = Math.max(this.y, other.y);
-            float ymax1 = this.y + this.getHeight();
-            float ymax2 = other.y + other.getHeight();
+            float ymax1 = this.y + this.getHeight(isSpace);
+            float ymax2 = other.y + other.getHeight(isSpace);
             float ymax = Math.min(ymax1, ymax2);
             if (ymax > ymin) {
                 return true;
--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -16,6 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Slf4j
 public class GameController {
@@ -41,15 +42,26 @@ public class GameController {
     @FXML
     private Button endPlacementButton;
 
+    @FXML
+    private Button startOwnButton;
+
+    @FXML
+    private Button startEnemyButton;
+
+
     private List<Ship> ownShips;
     private List<Ship> enemyShips;
     private HashMap<Square, Image> squares;
+    private List<Integer> hit;
+    private List<Integer> miss;
 
     @FXML
     public void initialize() {
         ownShips = new ArrayList<Ship>();
         enemyShips = new ArrayList<Ship>();
         squares = new HashMap<Square, Image>();
+        hit = new ArrayList<Integer>();
+        miss = new ArrayList<Integer>();
 
         squares.put(Square.SQUARE0, new Image(this.getClass().getResource("/img/square0.png").toExternalForm()));
         squares.put(Square.SQUARE1, new Image(this.getClass().getResource("/img/square1.png").toExternalForm()));
@@ -64,6 +76,36 @@ public class GameController {
         usernameLabel2.setText(userName2);
     }
 
+    private void clearOwnShips(){
+        for (Ship s : ownShips) {
+            int startX = s.getX() * 10 + s.getY();
+            for (int x = 0; x < s.getSize(); x++) {
+
+                int index = s.getDirection() == 1 ? startX + x : startX + (10 * x);
+                if (index >= 100)
+                    continue;
+
+                ImageView view = (ImageView) ownGrid.getChildren().get(index);
+                view.setImage(squares.get(Square.SQUARE0));
+            }
+        }
+    }
+
+    private void clearEnemyShips(){
+        for (Ship s : enemyShips) {
+            int startX = s.getX() * 10 + s.getY();
+            for (int x = 0; x < s.getSize(); x++) {
+
+                int index = s.getDirection() == 1 ? startX + x : startX + (10 * x);
+                if (index >= 100)
+                    continue;
+
+                ImageView view = (ImageView) enemyGrid.getChildren().get(index);
+                view.setImage(squares.get(Square.SQUARE0));
+            }
+        }
+    }
+
     private void drawOwnShips(){
         for (Ship s : ownShips) {
             int startX = s.getX() * 10 + s.getY();
@@ -75,6 +117,7 @@ public class GameController {
 
                 ImageView view = (ImageView) ownGrid.getChildren().get(index);
                 view.setImage(squares.get(Square.SQUARE2));
+
             }
         }
     }
@@ -93,7 +136,7 @@ public class GameController {
         }
     }
 
-    private boolean isEmptySpace(List<Ship> ships, Ship ship) {
+    private boolean isEmptySpace(List<Ship> ships, Ship ship, boolean isSpace) {
         if (horizontal.isDisable()) {
             if (10 - ship.getY() < ship.getSize()) {
                 log.warn("Ship cant fit..");
@@ -107,28 +150,31 @@ public class GameController {
         }
 
         for (Ship s : ships) {
-            if (s.inShip(ship))
+            if (s.inShip(ship, isSpace))
                 return false;
         }
         return true;
     }
+
     int stage = 0;
+    boolean prepPhase = true;
 
     public void endPlacementTurn() {
         if(stage == 10) {
             ownGrid.setVisible(false);
             stage++;
-            System.out.println("Stage: " + stage);
+            log.info("Stage: " + stage);
         }else if (stage == 21) {
             enemyGrid.setVisible(false);
-            ownGrid.setVisible(true);
+            ownGrid.setVisible(false);
             stage++;
             endPlacementButton.setVisible(false);
             horizontal.setVisible(false);
             vertical.setVisible(false);
-            System.out.println("Stage:" + stage);
+            log.info("Stage:" + stage);
+            prepPhase = false;
+            startOwnButton.setVisible(true);
         }
-
         endPlacementButton.setDisable(true);
     }
 
@@ -136,76 +182,120 @@ public class GameController {
 
         int clickedColumn = GridPane.getColumnIndex((Node)mouseEvent.getSource());
         int clickedRow = GridPane.getRowIndex((Node)mouseEvent.getSource());
-        System.out.println("own grid, (" + clickedRow + "," + clickedColumn + ")");
+        //log.info("own grid, (" + clickedRow + "," + clickedColumn + ")");
         int size = 0;
-
-        if (stage == 0) {
-            size = 4;
-        } else if (stage == 1 || stage == 2) {
-            size = 3;
-        } else if (stage >= 3 && stage <= 5) {
-            size = 2;
-        } else if (stage >= 6 && stage <=9) {
-            size = 1;
-        } else{
-            System.out.println("Can't place any more!");
-            return;
+        if(prepPhase) {
+            if (stage == 0) {
+                size = 4;
+            } else if (stage == 1 || stage == 2) {
+                size = 3;
+            } else if (stage >= 3 && stage <= 5) {
+                size = 2;
+            } else if (stage >= 6 && stage <= 9) {
+                size = 1;
+            } else {
+                log.warn("Can't place any more!");
+                return;
+            }
         }
 
         Ship ship = new Ship(size, horizontal.isDisable() ? 1 : 2, clickedRow, clickedColumn);
-        if (!isEmptySpace(ownShips, ship)) {
+        if (!isEmptySpace(ownShips, ship, true)) {
             log.warn("Ship in line!!");
             return;
         }
         ownShips.add(ship);
         drawOwnShips();
         stage++;
-        System.out.println("Stage: " + stage);
+        log.info("Stage: " + stage);
 
         if (stage == 10){
             endPlacementButton.setDisable(false);
         }
     }
 
-
     public void squareClickEnemy(MouseEvent mouseEvent) {
 
         int clickedColumn = GridPane.getColumnIndex((Node)mouseEvent.getSource());
         int clickedRow = GridPane.getRowIndex((Node)mouseEvent.getSource());
 
-        System.out.println("enemy grid, (" + clickedRow + "," + clickedColumn + ")");
+        //log.info("enemy grid, (" + clickedRow + "," + clickedColumn + ")");
+
+        if(stage>=22 && !prepPhase){
+            Ship fire = new Ship(1, 1, clickedRow, clickedColumn);
+            int index = clickedRow * 10 + clickedColumn;
+            if (hit.contains(index) || miss.contains(index)) {
+                log.warn("You already hit that!");
+                return;
+            }
+
+            if (!isEmptySpace(enemyShips, fire, false)) {
+                log.info("Hit!");
+                hit.add(index);
+                System.out.println(hit);
+                ImageView view = (ImageView) enemyGrid.getChildren().get(index);
+                view.setImage(squares.get(Square.SQUARE3));
+
+            } else{
+                log.info("Miss.");
+                miss.add(index);
+                System.out.println(miss);
+                ImageView view = (ImageView) enemyGrid.getChildren().get(index);
+                view.setImage(squares.get(Square.SQUARE1));
+            }
+        }
 
         int size = 0;
 
-        if (stage < 11){
-            log.warn("It's not your turn yet!");
-            return;
-        }
-        if (stage == 11) {
-            size = 4;
-        } else if (stage == 12 || stage == 13) {
-            size = 3;
-        } else if (stage >= 14 && stage <= 16) {
-            size = 2;
-        } else if (stage >= 17 && stage <=20) {
-            size = 1;
-        } else {
-            System.out.println("Can't place any more!");
-            return;
+        if (prepPhase) {
+            if (stage < 11) {
+                log.warn("It's not your turn yet!");
+                return;
+            }
+            if (stage == 11) {
+                size = 4;
+            } else if (stage == 12 || stage == 13) {
+                size = 3;
+            } else if (stage >= 14 && stage <= 16) {
+                size = 2;
+            } else if (stage >= 17 && stage <= 20) {
+                size = 1;
+            } else {
+                log.info("Can't place any more!");
+                return;
+            }
         }
 
         Ship ship = new Ship(size, horizontal.isDisable() ? 1 : 2, clickedRow, clickedColumn);
-        if (!isEmptySpace(enemyShips, ship)) {
-            log.warn("Ship in line!!");
-            return;
+        if (prepPhase) {
+            if (!isEmptySpace(enemyShips, ship, true)) {
+                log.warn("Ship in line!!");
+                return;
+            }
+
+            enemyShips.add(ship);
+            drawEnemyShips();
+            stage++;
+            log.info("Stage: " + stage);
+            if (stage == 21) {
+                endPlacementButton.setDisable(false);
+            }
+
         }
-        enemyShips.add(ship);
-        drawEnemyShips();
-        stage++;
-        System.out.println("Stage: " + stage);
-        if (stage == 21){
-            endPlacementButton.setDisable(false);
-        }
+    }
+
+    public void startOwn(){
+        clearEnemyShips();
+        ownGrid.setVisible(true);
+        enemyGrid.setVisible(true);
+        startOwnButton.setVisible(false);
+    }
+
+    public void startEnemy(){
+        clearEnemyShips();
+        enemyGrid.setVisible(true);
+        ownGrid.setVisible(true);
+        startEnemyButton.setVisible(false);
     }
 
     public void horizontalRelease() {
